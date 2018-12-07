193916cbeb69d84da5e2c141837aa08fa53e20e8
fujikawa-t <f.taisuke.numazu@gmail.com>
2018-12-07 21:07:53 +0900

    Initial commit

---
+++ app/src/main/java/tutorial/fujikawa/fuller/tutorial/MainActivity.kt
@@ -0,0 +1,46 @@
+package tutorial.fujikawa.fuller.tutorial
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ValueEventListener
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val key:String = "text"
+        val database = FirebaseDatabase.getInstance()
+        val ref = database.getReference(key)
+        ref.addValueEventListener(object: ValueEventListener {
+            var list : MutableList<String> = mutableListOf()
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                val values : Any? = dataSnapshot.value
+                val text : String = values.toString()
+                /*
+                var value : Map<String,String>? = values as? Map<String,String>
+                list.add(value.toString())
+                var text:String = value?.getOrDefault("2","hello").toString()
+                */
+                list.add(text)
+                val adapter = ArrayAdapter<String>(this@MainActivity, android.R.layout.simple_list_item_1, list)
+                view.setAdapter(adapter)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                Log.w("onCancelled", "error:", error.toException())
+            }
+        })
+
+
+    }
+
+
+}
